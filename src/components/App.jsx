8955749703b940ce2f14b6e732f26377942c8063
ndnastia <ndnastiadatsiuk@gmail.com>
2023-06-12 18:42:50 +0200

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import style from "./App.module.css"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={style['app-container']}>
+      
     </div>
   );
 };
