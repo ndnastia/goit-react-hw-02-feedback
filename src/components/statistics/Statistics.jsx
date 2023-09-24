e8293125fcca612ad6eca0dc13422e15986e10f6
ndnastia <ndnastiadatsiuk@gmail.com>
2023-09-24 23:41:50 +0200

    fixed total

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,11 +1,11 @@
 
 
-const Statistics = ({good, bad, neutral, totalFeedback, positivePercentage}) => {
+const Statistics = ({good, bad, neutral, total, positivePercentage}) => {
    return (<ul>
       <li>Good: {good}</li>
       <li>Neautral: {neutral}</li>
       <li>Bad: {bad}</li>
-      <li>Total: {totalFeedback}</li>
+      <li>Total: {total}</li>
       <li>Positive Feedback: {positivePercentage}</li>
    </ul>)
 }
